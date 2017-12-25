78e80ca66e4ccab23283aa528a09c580b917b6e7
jdcc1024 <34732640+jdcc1024@users.noreply.github.com>
2017-12-25 11:27:26 -0800

    Test Code
    Starting first draft

---
+++ test.js
@@ -0,0 +1,89 @@
+// We need this to build our post string
+var querystring = require('querystring');
+var http = require('http');
+var fs = require('fs');
+var request = require('request');
+
+var sqlQuery = "select%20*";
+var urlQuery = "gviz/tq?tq=" + sqlQuery;
+var rosterHash = "1uIGWI3CzdNPlXPgHvt1hLBu9-BLSAUK32AsGZoFDVlI";
+var waiverHash = "";
+var myURL = 'https://docs.google.com/spreadsheets/d/' + rosterHash + '/' + urlQuery;
+
+
+console.log("Send request to " + myURL);
+var rosterList = request.get(
+    myURL,
+    function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+        	console.log("Hello!!");
+            console.log(body)
+            jsonResp = JSON.parse(body);
+            console.log("return: " + jsonResp);
+
+        }
+    }
+);
+
+
+
+
+
+// function PostCode(codestring) {
+//   // Build the post string from an object
+//   var post_data = querystring.stringify({
+//       'compilation_level' : 'ADVANCED_OPTIMIZATIONS',
+//       'output_format': 'json',
+//       'output_info': 'compiled_code',
+//         'warning_level' : 'QUIET',
+//         'js_code' : codestring
+//   });
+
+//   // An object of options to indicate where to post to
+//   var post_options = {
+//       host: 'closure-compiler.appspot.com',
+//       port: '80',
+//       path: '/compile',
+//       method: 'POST',
+//       headers: {
+//           'Content-Type': 'application/x-www-form-urlencoded',
+//           'Content-Length': Buffer.byteLength(post_data)
+//       }
+//   };
+
+//   // Set up the request
+//   var post_req = http.request(post_options, function(res) {
+//       res.setEncoding('utf8');
+//       res.on('data', function (chunk) {
+//           console.log('Response: ' + chunk);
+//       });
+//   });
+
+//   // post the data
+//   post_req.write(post_data);
+//   post_req.end();
+
+// }
+
+
+
+// This is an async file read
+// fs.readFile('LinkedList.js', 'utf-8', function (err, data) {
+//   if (err) {
+//     // If this were just a small part of the application, you would
+//     // want to handle this differently, maybe throwing an exception
+//     // for the caller to handle. Since the file is absolutely essential
+//     // to the program's functionality, we're going to exit with a fatal
+//     // error instead.
+//     console.log("FATAL An error occurred trying to read in the file: " + err);
+//     process.exit(-2);
+//   }
+//   // Make sure there's data before we post it
+//   if(data) {
+//     PostCode(data);
+//   }
+//   else {
+//     console.log("No data to post");
+//     process.exit(-1);
+//   }
+// });
